b310d025c9a1a566e9adfdae0379e76d659f0698
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-06 01:48:25 +0000

    [応用課題8b]投稿数グラフ化

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,11 @@ class Book < ApplicationRecord
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_two_days_ago, -> { where(created_at: 2.day.ago.all_day) }
+  scope :created_three_days_ago, -> { where(created_at: 3.day.ago.all_day) }
+  scope :created_four_days_ago, -> { where(created_at: 4.day.ago.all_day) }
+  scope :created_five_days_ago, -> { where(created_at: 5.day.ago.all_day) }
+  scope :created_six_days_ago, -> { where(created_at: 6.day.ago.all_day) }
 end
