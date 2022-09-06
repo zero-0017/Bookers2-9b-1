fc4fe0973f5ba2d2fbaa1248a81f92ef190fb6da
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-06 01:44:24 +0000

    [応用課題7b]投稿数表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+
 end
